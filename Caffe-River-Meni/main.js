0738536e896338e870d6e63317a2b983a47c9fd9
Nikola D <95870159+Nikola1232456@users.noreply.github.com>
2022-07-22 00:49:33 +0200

    Add files via upload

---
+++ Caffe-River-Meni/main.js
@@ -0,0 +1,33 @@
+// preloader meni
+
+var loader = document.getElementById('preloader');
+
+        window.addEventListener('load', function(){
+            loader.style.display = 'none';
+})
+
+
+// back to top button
+
+mybutton = document.getElementById("myBtn");
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0; 
+}
+
+
+AOS.init();
+
+
